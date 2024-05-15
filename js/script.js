42855588f092eebca302afbd1468e8907a64af13
sergeishel123 <agaverdyevsergej@gmail.com>
2024-05-15 18:07:16 +0300

    Исправление формата сетки/ добавление возможности создания поля размерности от 3до 8

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 
-let main = document.getElementById("container");
-
-let cells = document.getElementsByClassName("cells");
-
-
-let square;
+let currentPlayer = 'X';
+let gridSize = 3;
+let gameBoard = [];
 
 
+document.getElementById("submit").addEventListener("click",createGrid);
+
+function createGrid() {
+    gridSize = Number(document.getElementById('gridSize').value);
+    gameBoard = [];
+    for (let i = 0; i < gridSize; i++) {
+        gameBoard[i] = new Array(gridSize);
+        for(let j = 0;j < gridSize;j++){
+            gameBoard[i][j] = "";
+        }
+    }
+    generateBoard();
+}
+
+function generateBoard(){
+    const board_html = document.getElementById('board_html');
+    board_html.innerHTML = "";
+    let table = document.createElement("table");
+    let row;
+    let col;
+    for(let i = 0;i < gridSize;i++){
+        row = document.createElement("tr");
+        for(let j = 0;j < gridSize;j++){
+            col = document.createElement("td");
+            col.textContent = gameBoard[i][j];
+            row.appendChild(col);
+            col.addEventListener("clicl",function(){this.innerHTML = "0"});
+        }
+        table.appendChild(row);
+    }
+    board_html.appendChild(table);
+}
+
+
+createGrid();
